5ce41c254d9bd7a13cb2ee64bf07a98402011419
kieudoannam <kieunam2808@gmail.com>
2022-12-05 15:14:03 +0700

    create Course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -3,6 +3,8 @@ const {multiMongooseToObject} = require('../../ulti/mongoose');
 const {mongooseToObject} = require('../../ulti/mongoose');
 
 class CourseController {
+
+    //[GET] courses/:slug
     slug(req, res,next) {
         Course.findOne({slug: req.params.slug})
             .then(course=> {
@@ -11,14 +13,30 @@ class CourseController {
             .catch(next)
     };
 
+    
+    //[GET] courses/
     index(req,res,error){
         Course.find({})
-            .then(courses=>{
-                //convert object contructor of mongoose to object inerral
-                res.render('courses',{ courses: multiMongooseToObject(courses) })
-            })
-            .catch(error => next(error))
+        .then(courses=>{
+            //convert object contructor of mongoose to object inerral
+            res.render('courses',{ courses: multiMongooseToObject(courses) })
+        })
+        .catch(error => next(error))
     }
+    
+    //[GET] courses/create
+    create(req, res,next) {
+        res.render('courses/create')
+     };
+
+    //[POST] courses/store
+    store(req, res,next) {
+        const course = Course(req.body);
+        course.save()
+            .then(()=> res.redirect(`/courses/${req.body.slug}`))
+            .catch(next)
+    };
+
 }
 
 module.exports = new CourseController();
--- src/app/modals/course.js
+++ src/app/modals/course.js
@@ -1,16 +1,16 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Course = new Schema({
-  name: {type: String, maxLength: 255},
+  name: {type: String, maxLength: 255, required: true},
   description: {type: String, maxLength: 600},
   image: {type: String, maxLength: 255},
-  createAt: {type: Date, default: Date.now},
-  updateAt: {type: Date, default: Date.now},
   slug: {type: String, maxLength: 255},
-  videoId: {type: String, maxLength: 255}
+  videoId: {type: String, maxLength: 255},
+},{
+  timestamps:true
 });
 
 module.exports = mongoose.model("Course",Course);
